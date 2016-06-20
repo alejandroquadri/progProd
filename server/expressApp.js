4cd2d10742c496b1fba82016f55c61c979b8bef1
Alejandro Quadri <alejandroquadri@gmail.com>
2016-06-20 14:00:01 -0300

    instalado angular a traves de bower

--- server/expressApp.js
+++ server/expressApp.js
@@ -6,6 +6,7 @@ var app = express();
 // });
 
 app.use( express.static(__dirname + "/../public"))
+app.use('/bower', express.static(__dirname + "/../bower_components"));
 
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
