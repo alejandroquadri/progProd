eb835a9ab3b7cc3fedf469f5e55e1ab6b639b333
Alejandro Quadri <alejandroquadri@gmail.com>
2016-07-30 16:41:35 -0300

    desinstalado cosas de node que no usaba y arreglado CSS

--- public/js/controllers/entregas.js
+++ public/js/controllers/entregas.js
@@ -1,6 +1,6 @@
 angular.module('ProgProd')
 
-.controller('Entregas', function($scope, gSS){
+.controller('Entregas', function($scope, gSS, $window){
   console.log('arranca controlador Entregas');
   $scope.datos = gSS.dataSS
 
@@ -8,5 +8,11 @@ angular.module('ProgProd')
   $scope.semanas = [semanaActual, semanaActual + 1, semanaActual + 2, semanaActual + 3]
   $scope.items = Object.keys($scope.datos)
 
+  $scope.print = function(){
+   var table = document.getElementById('printArea').innerHTML;
+   var myWindow = $window.open('', '', 'width=800, height=600');
+   myWindow.document.write(table);
+   myWindow.print();
+  };
 
 })
